b8f9f9ba27793bd37e9ce1a37ed1f4abad1ab318
Aleksandr Aniskin <aleksandr.nskn@yandex.ru>
2023-02-16 12:03:40 +0300

    Initial Commit

---
+++ SimpleWeather/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleWeather
+//
+//  Created by Aleksandr Aniskin on 16.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
